bea49be53ab3bf79e8686b4ed7c585d4d427caeb
karol <karoll.gajda@gmail.com>
2014-12-23 23:43:06 +0100

    add script

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+PID=$(ps aux | grep '[j]ava -jar build/libs/kllegro-0.1.0.jar' | awk '{print $2}')
+if [ -z "$PID" ]
+then
+    echo "Application is already stopped"
+else
+    echo "kill process:" $PID
+    kill $PID
+fi
+
+java -jar build/libs/kllegro-0.1.0.jar &
+
+PID=$(ps aux | grep '[j]ava -jar build/libs/kllegro-0.1.0.jar' | awk '{print $2}')
+
+echo "Application PID:" $PID
